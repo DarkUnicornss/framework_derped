64ca5eb4783ee010fc690b28503565d43ab43813
rk2810 <rohan.kumquat@gmail.com>
2017-12-05 22:04:25 +0000

    Revert "HAX : make dt2s on lockscreen work"
    This reverts commit a76c30f65a8576dd67b303ee9bc40554b45f9c40.

--- core/java/com/android/internal/util/du/Utils.java
+++ core/java/com/android/internal/util/du/Utils.java
@@ -16,6 +16,11 @@
 
 package com.android.internal.util.du;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.Intent;
+import android.os.UserHandle;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageInfo;
@@ -24,7 +29,11 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.hardware.fingerprint.FingerprintManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-
+import android.hardware.input.InputManager;
+import android.view.InputDevice;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.WindowManager;
 import com.android.internal.R;
 
 public class Utils {
@@ -114,5 +123,30 @@ public class Utils {
     // Check to see if device supports an alterative ambient display package
     public static boolean hasAltAmbientDisplay(Context context) {
         return context.getResources().getBoolean(com.android.internal.R.bool.config_alt_ambient_display);
+    }
+
+    /**
+     * @hide
+     */
+    public static final String SYSTEMUI_PACKAGE_NAME = "com.android.systemui";
+
+    /**
+     * @hide
+     */
+    public static final String ACTION_DISMISS_KEYGUARD = SYSTEMUI_PACKAGE_NAME +".ACTION_DISMISS_KEYGUARD";
+
+    /**
+     * @hide
+     */
+    public static final String DISMISS_KEYGUARD_EXTRA_INTENT = "launch";
+
+    /**
+     * @hide
+     */
+    public static void launchKeyguardDismissIntent(Context context, UserHandle user, Intent launchIntent) {
+        Intent keyguardIntent = new Intent(ACTION_DISMISS_KEYGUARD);
+        keyguardIntent.setPackage(SYSTEMUI_PACKAGE_NAME);
+        keyguardIntent.putExtra(DISMISS_KEYGUARD_EXTRA_INTENT, launchIntent);
+        context.sendBroadcastAsUser(keyguardIntent, user);
     }
 }
